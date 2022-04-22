fc6c2baf48e40a7617b8f7343393d3c78b733e7e
blueThunder <91856239+blueThunder369@users.noreply.github.com>
2022-04-22 18:41:29 -0400

    Update order-form.js

--- order-form.js
+++ order-form.js
@@ -17,7 +17,7 @@
     function isChecked() {
         for (let i = 0; i < checkboxLength; i++) {
             if (checkboxes[i].checked) return true;
-            window.open("index.html","_self")
+
         }
 
         return false;
@@ -27,6 +27,56 @@
         const errorMessage = !isChecked() ? 'At least one checkbox must be selected.' : '';
         firstCheckbox.setCustomValidity(errorMessage);
     }
-
+    
     init();
 })();
+
+   </script>
+   
+   <script>
+   window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("my-form");
+  // var button = document.getElementById("my-form-button");
+  var status = document.getElementById("status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Thanks!";
+    window.open("https://bluethunder369.github.io/3d-printing-shop/index.html","_self")
+  }
+
+  function error() {
+    status.classList.add("error");
+    status.innerHTML = "Oops! There was a problem.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
